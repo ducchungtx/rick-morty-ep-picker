236eb32865137b0432db940d911b0aac4f24d684
Sophia Brandt <tech@s-br.net>
2019-04-09 12:55:10 +0200

    Updates

---
+++ precache-manifest.2b9e295c6420c61b7bd612141d6f1155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea2de28607882fcea7b4",
+    "url": "/rick-morty-ep-picker/static/js/0.310686b2.chunk.js"
+  },
+  {
+    "revision": "fbe2ec831e9301230636",
+    "url": "/rick-morty-ep-picker/static/css/main.011c6566.chunk.css"
+  },
+  {
+    "revision": "fbe2ec831e9301230636",
+    "url": "/rick-morty-ep-picker/static/js/main.a84a5742.chunk.js"
+  },
+  {
+    "revision": "0459edc24e12eadd6b46",
+    "url": "/rick-morty-ep-picker/static/js/runtime~main.3ce38c39.js"
+  },
+  {
+    "revision": "5de8c200fc8ae25c3638",
+    "url": "/rick-morty-ep-picker/static/js/3.1f052622.chunk.js"
+  },
+  {
+    "revision": "5085c18c17938f385df69900b0612e46",
+    "url": "/rick-morty-ep-picker/index.html"
+  }
+];
